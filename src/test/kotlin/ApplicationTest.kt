9e3d4c274a9cd899158dd3598a936445b9165fa3
ysa2shi <satoshi.1109.work@gmail.com>
2026-05-02 22:42:14 +0900

    first commit

---
+++ src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package com.example
+
+import io.ktor.client.request.get
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.testApplication
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            module()
+        }
+        client.get("/health").apply {
+            assertEquals(HttpStatusCode.OK, status)
+        }
+    }
+
+}
